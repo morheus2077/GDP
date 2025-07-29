8414e9961473e293b70198e1709a30b572c4d3dd
Emílio Marcelino Honório Branquinho <emiliomarcelino400@gmail.com>
2025-07-29 16:29:59 -0700

    s

--- src/App.tsx
+++ src/App.tsx
@@ -188,7 +188,7 @@ function App() {
           {/* Card 2 */}
           <div className='bg-white border border-gray-300 mx-3 md:w-3xl lg:w-96 h-32 md:h-36 rounded-lg my-5'>
             <div className='flex justify-between px-5 pt-5'>
-              <div className='md:text-lg'>Número de DespesasTESTE</div>
+              <div className='md:text-lg'>Número de DespesasTESTES</div>
               <div className='text-gray-600'><Calculator/></div>
             </div>
             <div className='px-5 pt-3 md:pt-10 text-2xl font-bold'>
